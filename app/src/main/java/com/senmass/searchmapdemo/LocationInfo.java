e41a8b5d8dc9af504fbc7617263099c82f411bb0
senmass <1725318486@qq.com>
2020-08-27 13:51:06 +0800

    Initial commit

---
+++ app/src/main/java/com/senmass/searchmapdemo/LocationInfo.java
@@ -0,0 +1,32 @@
+package com.senmass.searchmapdemo;
+
+
+public class LocationInfo {
+    private String address;
+    private Double Latitude;
+    private Double LonTitude;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Double getLatitude() {
+        return Latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        Latitude = latitude;
+    }
+
+    public Double getLonTitude() {
+        return LonTitude;
+    }
+
+    public void setLonTitude(Double lonTitude) {
+        LonTitude = lonTitude;
+    }
+}
